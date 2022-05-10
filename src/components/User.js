8371654af9145200ddeb5e91693fab85796fac65
Denis Bao <denis.tpd@gmail.com>
2022-05-10 16:34:29 -0300

    165_Adding a First Class-based Component

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,19 @@
-import classes from './User.module.css';
+import  { Component } from 'react'
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from './User.module.css';
+class User extends Component{
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>
+  }
+}
 
 export default User;
+
+
+// FUNCTION VERSION OF THE COMPONENT =========================
+//
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+// export default User;
